e19afc7b8469164e06ea85c7e00ee475a5ed7394
ryuj7338 <ryuj7338@gmail.com>
2025-03-20 16:00:34 +0900

    1번 문제

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            System.out.println("****");
+       }
+        for (int i = 0; i < a; i++) {
+            System.out.print("");
+        }
+
+    }
+}
